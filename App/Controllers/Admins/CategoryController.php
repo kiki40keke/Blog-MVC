8f41e2945241d7401347ef67a96cb80b54dc9479
Laguerre kelly Kensky <laguerrekelly.k@gmail.com>
2025-10-11 01:24:34 -0400

    feat(category): implement new method and view for creating categories

--- App/Controllers/Admins/CategoryController.php
+++ App/Controllers/Admins/CategoryController.php
@@ -10,8 +10,10 @@ use App\Core\Connection;
 use App\Helpers\Session;
 use App\Helpers\Hydrator;
 use App\Models\Entity\Post;
+use App\Models\Entity\Category;
 use App\Validators\PostValidator;
 use App\Controllers\BaseController;
+use App\Validators\CategoryValidator;
 use App\Models\Repository\PostRepository;
 use App\Models\Repository\CategoryRepository;
 
@@ -27,5 +29,43 @@ class CategoryController extends BaseController
         [$categories, $paginatedquery] = $table->findPaginatedbyCategory();
         $link = $this->router->url('admin_categories');
         return $this->render('admins/category/index', compact('title', 'categories', 'paginatedquery', 'link', 'active'));
+    }
+
+    public function new(): string
+    {
+        $link = $this->router->url('admin_categories');
+
+
+        $errors = [];
+        $category = new Category();
+
+        if (!empty($_POST)) {
+            //dd($post->getId());
+            $pdo = Connection::getPDO();
+
+            $table = new CategoryRepository($pdo);
+
+            $v = new CategoryValidator($_POST, $table);
+
+            if ($v->validate()) {
+
+                //dd($created_at);
+
+                Hydrator::hydrate($category, $_POST, ['name', 'slug']);
+
+                $id = $table->insertCategory($category);
+                // dd($id);
+                Session::setFlash('success', 'la categorie a bien été créé ✅');
+                header('Location: ' . $link);
+                exit;
+            } else {
+                $errors = $v->errors();
+            }
+        }
+
+        $form = new Form($category, $errors);
+        $title = "Nouvelle categorie";
+        $active = 'categories';
+        return $this->render('admins/category/new', compact('title', 'form', 'errors', 'link', 'active'));
     }
 }
---
+++ App/Views/admins/category/new.php
@@ -0,0 +1,10 @@
+<h1 class="text-center">Enregistrement </h1>
+
+
+
+<?php if (!empty($errors)): ?>
+    <div class="alert alert-danger">
+        c'est pas enregistrer
+    </div>
+<?php endif ?>
+<?php require 'form.php';
