6e1383e72e8ab55da10957e13103ce2598a8eabe
Laguerre kelly Kensky <laguerrekelly.k@gmail.com>
2025-10-11 02:39:45 -0400

    refactor(category): remove unused imports in CategoryController for cleaner code

--- App/Controllers/Admins/CategoryController.php
+++ App/Controllers/Admins/CategoryController.php
@@ -3,18 +3,12 @@
 namespace App\Controllers\Admins;
 
 use App\HTML\Form;
-use App\Helpers\Date;
-use App\Helpers\Text;
-use App\Helpers\Upload;
 use App\Core\Connection;
 use App\Helpers\Session;
 use App\Helpers\Hydrator;
-use App\Models\Entity\Post;
 use App\Models\Entity\Category;
-use App\Validators\PostValidator;
 use App\Controllers\BaseController;
 use App\Validators\CategoryValidator;
-use App\Models\Repository\PostRepository;
 use App\Models\Repository\CategoryRepository;
 
 class CategoryController extends BaseController
